3c7f4594390b82096255629501ae7f215b906b64
Ely Cassimiro <Ely Cassimiro@DESKTOP-0UPTIB3>
2024-10-05 17:36:16 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Models/Relatorio.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Relatorio
+
+        [Key]
+    {
+        public int Id { get; set; }
+        [Required]
+        
+        public  DateTime  Data { get; set; }
+    }
+}
+        
+        
